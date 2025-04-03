438b5bafc8368d1b181ac4e1ecde9829dd9dda09
beleenco <belencamposolmo@gmail.com>
2025-04-03 19:21:04 +0200

    Commit download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -16,3 +16,31 @@
 #   CCAGGATTTACAGACTTTAAA
 #
 #   If $4 == "another" only the **first two sequence** should be output
+
+file=$1
+dir=$2
+myvar=$3
+
+
+if [ "$#" -gt 1 ]
+then 
+    # Go to directory and download file
+    wget -P ${dir} ${file}
+    filename=$(basename ${file})
+
+    # Uncompress file if 'yes' is specified
+    if [ "$myvar" == "yes" ]
+    then
+        gunzip -k ${dir}/${filename}
+    fi
+
+    # Filter sequences based on word in header (if $4 is provided)
+    if [ -n "$4" ]; then
+        gunzip -c ${file} | more | "/$4/{N;d;}" > "${filename%.fastq.gz}filtered.fastq.gz"
+        
+    fi
+
+else
+    echo "Error: this script should be run with 2 or more arguments"
+fi
+
