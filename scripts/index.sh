ac6e42e59854264b623f58f095313eefcdf30967
beleenco <belencamposolmo@gmail.com>
2025-04-03 19:21:28 +0200

    Commit index.sh

--- scripts/index.sh
+++ scripts/index.sh
@@ -4,5 +4,7 @@
 # The STAR command is provided for you. You should replace the parts surrounded
 # by "<>" and uncomment it.
 
-# STAR --runThreadN 4 --runMode genomeGenerate --genomeDir <outdir> \
-# --genomeFastaFiles <genomefile> --genomeSAindexNbases 9
+output_dir=$2
+genome=$1
+ STAR --runThreadN 4 --runMode genomeGenerate --genomeDir ${output_dir} \
+ --genomeFastaFiles ${genome} --genomeSAindexNbases 9
