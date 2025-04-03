eb61dc388bcd57f8569175ca31920c6bcf6af3c1
beleenco <belencamposolmo@gmail.com>
2025-04-03 19:21:53 +0200

    Commit merge_fastqs.sh

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,12 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+
+sample_directory=$1
+output_directory=$2
+sid=$3
+
+mkdir -p ${output_directory}
+echo "Merging files for sample ${sid}..."
+cat ${sample_directory}/${sid}-12.5dpp.1.1s_sRNA.fastq.gz ${sample_directory}/${sid}-12.5dpp.1.2s_sRNA.fastq.gz > ${output_directory}/${sid}_merged.fastq.gz
+
